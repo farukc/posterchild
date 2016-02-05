28ede8768308f300dd141e85179dc1fd91ae24b4
Rachel Simone Weil <partytimehexcellent@gmail.com>
2016-02-05 05:49:07 -0800

    Alert placeholders

--- js/script.js
+++ js/script.js
@@ -2,13 +2,22 @@ $(function posterChild(){
 
     $('#main').addClass('fadingin1sec');
 
-    $('li').on('click', function() {if ($(this).attr('class') == 'selected')
+    $('li').on('click', function() { if ($(this).attr('class') == 'selected')
         { $(this).attr('class', 'notselected'); }
         else {
-            $('li').each(function() {$('li').attr('class','notselected');});
+            $('li').each(function() { $('li').attr('class','notselected');});
             $(this).attr('class', 'selected');
         }    
     });
+    
+    $('button').on('click', submissionChecker);
+    
+     function submissionChecker() {
+        if ($(".selected").length > 0) {
+            alert('Placeholder for generating poster.');
+        }
+        else { alert('Please select a poster.'); }    
+     }
 
 });
 
